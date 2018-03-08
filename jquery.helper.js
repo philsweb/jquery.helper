86f6e02613b729d4430687f08b25704328ea8566
Philipp <philipp.nikolajev@suomenpt.fi>
2018-03-08 11:57:50 +0200

    getScrollBarWidth() added

---
+++ jquery.helper.js
@@ -0,0 +1,6 @@
+$.getScrollBarWidth = function () {
+    var $outer = $('<div>').css({visibility: 'hidden', width: 100, overflow: 'scroll'}).appendTo('body'),
+        widthWithScroll = $('<div>').css({width: '100%'}).appendTo($outer).outerWidth();
+    $outer.remove();
+    return Math.round(100 - widthWithScroll);
+};
